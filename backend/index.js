311470e669e012246f28d1d34af48f52e7704ef4
Shubham Sareliya <sareliashubham9999@gmail.com>
2023-01-16 15:43:58 +0530

    Basic Express App Initiated

--- backend/index.js
+++ backend/index.js
@@ -1,1 +1,12 @@
-console.log("Backend Code starts from here")
+require('dotenv').config()
+const express = require('express')
+
+const app = express()
+
+app.get('/', (req, res) => {
+    res.send('Hello World!')
+})
+
+app.listen(process.env.PORT, () => {
+    console.log(`Example app listening at http://localhost:${process.env.PORT}`)
+})
